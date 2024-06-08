1e5565c3d9faf4858df9f781e906542fba6b3b5e
Renan Germano <61218420+renangfs@users.noreply.github.com>
2024-06-08 12:24:18 -0300

    actualization 08/06

--- src/main/java/org/example/ConnectionFactory.java
+++ src/main/java/org/example/ConnectionFactory.java
@@ -7,11 +7,25 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     public static Connection recuperarConexao() {
+        String url = "jdbc:mysql://34.31.153.165:3306/OFICINA";
+        String user = "root";
+        String password = "JQ6GZT@9r8QOU0Lc";
+
         try {
-            return DriverManager
-                    .getConnection("jdbc:mysql://35.199.78.153:3306/oficina", "root", "b2Y74Sk`kC,$leH#");
+            return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
+            System.err.println("Erro ao tentar se conectar ao banco de dados: " + e.getMessage());
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = recuperarConexao()) {
+            if (connection != null) {
+                System.out.println("Conexão estabelecida com sucesso!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import java.sql.Connection;
 
 public class Main{
     public static void main(String[] args) {
